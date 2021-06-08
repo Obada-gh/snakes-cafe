030c5f13a6ecf00d5ef97ced82e6263389b3dcea
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-06-08 09:20:18 +0300

    fixed

--- snakes_cafe/snakes_cafe.py
+++ snakes_cafe/snakes_cafe.py
@@ -1,19 +1,48 @@
-print('**************************************')
-print('**    Welcome to the Snakes Cafe!   **')
-print('**    Please see our menu below.    **')
-print('**')
-print('** To quit at any time, type "quit" **')
-print('**************************************')
-print('\nAppetizers\n----------\nWings\nCookies\nSpring Rolls')
-print('\nEntrees\n-------\nSalmon\nSteak\nMeat Tornado\nA Literal Garden')
-print('\nDesserts\n--------\nIce Cream\nCake\nPie')
-print('\nDrinks\n------\nCoffee\nTea\nUnicorn Tears')
-print('\n***********************************\n** What would you like to order? **\n***********************************')
-i=1
-while i< 999999:
-  userOrder = input('>')
-  
-  if userOrder == 'quit':
-      break
-  print(f'** {i} order of {userOrder} have been added to your meal **')  
-  i+=1
+print("""
+$ python snakes_cafe.py
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+""")
+oneOrder=""
+orders=[]
+
+while oneOrder != 'quit':
+  oneOrder = input('>')
+  if oneOrder != 'quit':
+    orders.append(oneOrder)
+    i=orders.count(oneOrder)
+    print(f'**{i} order of {oneOrder} added to your meal**')
+
